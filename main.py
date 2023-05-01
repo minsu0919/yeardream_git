cc6ccc0f412d085c1a97c80f8fe04caf1ebccf6e
minsu0919 <kmsb0919@naver.com>
2023-05-01 20:23:41 +0900

    feat : Define print_hello() (Trailing comma test)

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+def print_hello():
+    animals = ['dog','cat','hamster']
+    foods = [
+        'Spagetti',
+	'Pizza'
+    ]
+    names = [
+         'John',
+         'Jane',
+         'Gil-dong',
+    ]
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+if __name__ == '__main__':
+    print_hello()
